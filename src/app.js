8bad0ded3127d848063029e2166ec96c8a2deb3c
Dipayan Maji <121128467+dipayanmaji@users.noreply.github.com>
2023-04-18 18:00:43 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,13 @@ app.use(express.json());
 // Write a GET Request to get the next number from the input 'num'.
 // Endpoint : /api/get-next-num
 // Return the response as {message : , status: }
+app.get('/api/get-next-num', (req, res) => {
+    if (req.body.num && typeof req.body.num == "number") {
+        res.status(200).json({ "message": req.body.num + 1, "status": "success" })
+    }
+    else {
+        res.status(404).json({ "message": "error message", "status": "failure" })
+    }
+})
 
 module.exports = app;
